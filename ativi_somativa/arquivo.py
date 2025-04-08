76c34efd746719b4f1a48a13e5ea5d4c985278e8
Yasmin <yasmin.v.ramos@aluno.senai.br>
2025-04-08 08:45:53 -0300

    Arquivo incompleto
    Código com 5 linhas a menos

---
+++ ativi_somativa/arquivo.py
@@ -0,0 +1,14 @@
+print("Olá usuário, me diga a sua altura e seu peso que te direi se você está com um peso favorável.")
+
+a = float(input ("Me fale a sua altura: "))
+b = float(input("Me fale o seu peso: "))
+conta = b / (a * a)
+
+if   conta < 20:
+    print("Seu estado atual é abaixo do peso, está em magreza.")
+    
+elif conta >= 20 and conta <= 24.9:
+    print("Seu estado atual é de normalidade, pudinzinho!")
+     
+elif conta >= 25 and conta <= 29.9:
+
