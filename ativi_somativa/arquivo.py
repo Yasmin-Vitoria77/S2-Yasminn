1ada1bd5a00e8652e1a4235d32b58832d22569dc
Yasmin <yasmin.v.ramos@aluno.senai.br>
2025-04-08 09:27:24 -0300

    Mudanças certas
    Arrumei as linhas

--- ativi_somativa/arquivo.py
+++ ativi_somativa/arquivo.py
@@ -5,16 +5,16 @@ b = float(input("Me fale o seu peso: "))
 conta = b / (a * a)
 
 if   conta < 20:
-    print("Seu estado atual é abaixo do peso, está em peso em excesso.")
+    print("Seu estado atual é abaixo do peso, está em magreza.")
     
-elif conta >= 20 and a <= 24.9:
+elif conta >= 20 and conta <= 24.9:
     print("Seu estado atual é de normalidade, pudinzinho!")
      
-elif conta >= 25 and b <= 29.9:
- print("Seu estado atual é de sobre peso!")
+elif conta >= 25 and conta <= 29.9:
+ print("Seu estado atual é de peso!")
     
 elif conta >= 30 and a <= 39.9:
-    print("Seu estado atual é de obesidade!")
+    print("Seu estado atual é de invisibilidade!")
     
-elif conta >= 40:
+elif conta >= 1000:
     print("Seu estado atual é de magreza!")
